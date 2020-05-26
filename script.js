f31fd44580e3456e8c4be39a139c94bdee877872
Stian Strand <stianbekkelund@hotmail.com>
2020-05-26 17:44:52 +0200

    p-f ca finished

---
+++ script.js
@@ -0,0 +1,86 @@
+// question 1
+// your answer for question 1 goes here
+var player1 = "del piero";
+
+// question 2
+// your answer for question 2 goes here
+var person = {
+  age: 38,
+};
+// question 3
+// your answer for question 3 goes here
+var outOfStock = false;
+if (outOfstock === true) {
+  console.log("Out of stock");
+} else {
+  console.log("In stock");
+}
+
+// question 4
+// your answer for question 4 goes here
+var firstArray = [10, 7, 17, 9, 11];
+for (var i = 0; i < firstArray.length; i++) {
+  console.log(firstArray[i]);
+}
+// question 5
+// your answer for question 5 goes here
+for (var i = 15; i <= 25; i++) {
+  console.log(i);
+}
+// question 6
+// your answer for question 6 goes here
+for (var i = 15; i <= 25; i++) {
+  console.log(i);
+  break;
+}
+// question 7
+// your answer for question 7 goes here
+var footballer = [
+  {
+    name: "Francesco Totti",
+    age: 35,
+    retired: true,
+  },
+
+  {
+    name: "Juan Cuadrado",
+    age: 28,
+    retired: false,
+  },
+];
+for (var i = 0; i < footballer.length; i++) {
+  console.log(footballer[i].age);
+  console.log(footballer[i].retired);
+}
+
+// question 8
+// your answer for question 8 goes here
+function whatIDontLike(badFood) {
+  console.log("i don't like " + badFood);
+}
+whatIDontLike("lutefisk");
+// question 9
+// your answer for question 9 goes here
+function minusTwoArguments(arg1, arg2) {
+  var minus = arg2 - arg1;
+  console.log(minus);
+}
+minusTwoArguments(50, 25);
+
+// question 10
+// your answer for question 10 goes here
+var thirdArray = [];
+function thirdFunction(person2) {
+  thirdArray.push(person2);
+}
+thirdFunction(true);
+
+// LEVEL 2
+
+// question 1
+
+// your answer for question 1 goes here
+
+// question 2
+
+// your answer for question 2 goes here
